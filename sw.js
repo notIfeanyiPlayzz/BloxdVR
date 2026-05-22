8e4e2a7c52eeeea6a76a6aaf280273830bc0d4cf
IfeanyiPlayzz <isiguzoflorence521@gmail.com>
2026-05-22 09:52:13 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'bloxdvr-offline-v1';
+const OFFLINE_URL = '404.html';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.add(OFFLINE_URL))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request).catch(() => {
+                return caches.match(OFFLINE_URL);
+            })
+        );
+    }
+});
